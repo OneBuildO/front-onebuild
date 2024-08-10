e5b4502c35cba1aba3d3563d9a811e061a108909
vctrwesley <vctrwesley@gmail.com>
2024-08-10 19:42:17 -0300

    cadastro e validação de email funcionando novamente

--- src/app/public/auth/cadastro/cadastro.component.ts
+++ src/app/public/auth/cadastro/cadastro.component.ts
@@ -1,18 +1,18 @@
-import {Component} from '@angular/core';
-import {FormBuilder, FormControl, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
-import {DatetimeHelper} from 'src/app/_core/helpers/datetime.helper';
-import {CommonService} from 'src/app/_core/services/common.service';
-import {pageTransition} from 'src/app/shared/utils/animations';
-import {AlertType} from 'src/app/shared/components/alert/alert.type';
-import {PublicRoutes} from '../../public.routes';
-import {ETipoUsuario, TipoUsuarioArr} from "src/app/_core/enums/e-tipo-usuario";
-import {CadastroUsuarioDTO} from "src/app/_core/models/cadastro.model";
-import {UsuarioService} from "src/app/_core/services/usuario.service";
-import {AuthService} from "src/app/_core/services/auth.service";
-import {ERROR_MESSAGES} from "src/app/shared/components/validation-error/error-messages";
-import {CategoriasProjetoArr} from "src/app/_core/enums/e-categorias-projeto";
-import {EMessages} from "src/app/_core/enums/e-messages";
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DatetimeHelper } from 'src/app/_core/helpers/datetime.helper';
+import { CommonService } from 'src/app/_core/services/common.service';
+import { pageTransition } from 'src/app/shared/utils/animations';
+import { AlertType } from 'src/app/shared/components/alert/alert.type';
+import { PublicRoutes } from '../../public.routes';
+import { ETipoUsuario, TipoUsuarioArr } from "src/app/_core/enums/e-tipo-usuario";
+import { CadastroUsuarioDTO } from "src/app/_core/models/cadastro.model";
+import { UsuarioService } from "src/app/_core/services/usuario.service";
+import { AuthService } from "src/app/_core/services/auth.service";
+import { ERROR_MESSAGES } from "src/app/shared/components/validation-error/error-messages";
+import { CategoriasProjetoArr } from "src/app/_core/enums/e-categorias-projeto";
+import { EMessages } from "src/app/_core/enums/e-messages";
 
 @Component({
   selector: 'app-signup',
@@ -22,12 +22,13 @@ import {EMessages} from "src/app/_core/enums/e-messages";
 })
 export class CadastroComponent {
   constructor(
-      public commonService: CommonService,
-      private formBuilder: FormBuilder,
-      private router: Router,
-      private authService: AuthService,
-      private serviceUser : UsuarioService,
+    public commonService: CommonService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService,
+    private serviceUser: UsuarioService,
   ) {}
+
   protected readonly AlertType = AlertType;
   readonly publicRoutes = PublicRoutes;
   readonly currentYear: number = DatetimeHelper.currentYear;
@@ -39,79 +40,115 @@ export class CadastroComponent {
   tipoAlerta = AlertType.Warning;
 
   cadastroForm = this.formBuilder.group({
-    nome: new FormControl( '', { validators: [Validators.required] }),
-    tipoUsuario: new FormControl( '', { validators: [Validators.required] }),
-    categoria: new FormControl( '', { validators: [Validators.required] }),
-    email: new FormControl( '', { validators: [Validators.required] }),
-    contato: new FormControl( '', { validators: [Validators.required] }),
-    cnpj: new FormControl( '', { validators: [Validators.required] }),
-    senha: new FormControl( '', { validators: [Validators.required] }),
-    confirmPassword: new FormControl( '', { validators: [Validators.required] }),
+    nome: new FormControl('', { validators: [Validators.required] }),
+    tipoUsuario: new FormControl('', { validators: [Validators.required] }),
+    categoria: new FormControl(''), // Sem validação obrigatória inicialmente
+    email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+    contato: new FormControl('', { validators: [Validators.required] }),
+    cnpj: new FormControl('', { validators: [Validators.required] }),
+    senha: new FormControl('', { validators: [Validators.required, Validators.minLength(8)] }),
+    confirmPassword: new FormControl('', { validators: [Validators.required] }),
     terms: new FormControl(false, { validators: [Validators.requiredTrue] }) // Adicionando o campo de termos
   });
 
   urlParams = new URL(window.location.href);
   paramIdConvite = this.urlParams.searchParams.get("convite");
 
-  onFormSubmitHandler = (event: SubmitEvent) => {
-    event.preventDefault();
-    this.submited = true;
-    this.serverErrors = []
+  ngOnInit() {
+    this.cadastroForm.get('tipoUsuario')?.valueChanges.subscribe(value => {
+      if (value === ETipoUsuario.FORNECEDOR || value === ETipoUsuario.OUTROS) {
+        this.cadastroForm.get('categoria')?.setValidators([Validators.required]);
+      } else {
+        this.cadastroForm.get('categoria')?.clearValidators();
+      }
+      this.cadastroForm.get('categoria')?.updateValueAndValidity();
+    });
+  }
 
-    if(this.cadastroForm.invalid) return
-    if(!this.isSenhaValida()) return;
+  onFormSubmitHandler(event?: Event) {
+    console.log("Formulário submetido!");
+
+    if (event) {
+      event.preventDefault();
+    }
+
+    this.submited = true;
+    this.serverErrors = [];
+
+    console.log('Formulário inválido:', this.cadastroForm.invalid);
+    
+    // Log detalhado de cada controle
+    Object.keys(this.cadastroForm.controls).forEach(key => {
+        const control = this.cadastroForm.get(key);
+        console.log(`Controle: ${key} - Inválido: ${control?.invalid} - Erros:`, control?.errors);
+    });
+
+    if (this.cadastroForm.invalid) return;
+    if (!this.isSenhaValida()) return;
 
     this.isLoading = true;
 
-    const dadosCadastro : CadastroUsuarioDTO  = {
-      nome : this.cadastroForm.controls?.nome?.value,
-      tipoUsuario : this.cadastroForm.controls?.tipoUsuario?.value,
-      categoria : this.cadastroForm.controls?.categoria?.value,
-      email : this.cadastroForm.controls?.email?.value,
-      contato : this.cadastroForm.controls?.contato?.value,
-      cnpj : this.cadastroForm.controls?.cnpj?.value,
-      senha : this.cadastroForm.controls?.senha?.value,
-      convite : this.paramIdConvite
-    }
+    console.log("Todos os dados do formulário estão válidos, preparando para envio...");
 
-    this.serviceUser.saveUser(dadosCadastro)
-      .subscribe({
-        next: (data: any) => {
-          this.authService.handleSetToken(data);
-          window.location.href = `/validacao-email?email=${dadosCadastro.email}`
-        },
-        error: (err) => {
-          this.tipoAlerta = AlertType.Danger
-          if(err.statusText == "Unknown Error"){
-            this.serverErrors.push(EMessages.SERVER_ERROR)
-          }else{
-            this.serverErrors.push(err.error)
-          }
+    const dadosCadastro: CadastroUsuarioDTO = {
+      nome: this.cadastroForm.get('nome')?.value || '',
+      tipoUsuario: this.cadastroForm.get('tipoUsuario')?.value || '',
+      categoria: this.cadastroForm.get('categoria')?.value || '',
+      email: this.cadastroForm.get('email')?.value || '',
+      contato: this.cadastroForm.get('contato')?.value || '',
+      cnpj: this.cadastroForm.get('cnpj')?.value || '',
+      senha: this.cadastroForm.get('senha')?.value || '',
+      convite: this.paramIdConvite || ''
+    };
 
-          this.isLoading = false;
-          this.scrollTop()
+    console.log("Dados do cadastro preparados:", dadosCadastro);
+
+    this.serviceUser.saveUser(dadosCadastro).subscribe({
+      next: (data: any) => {
+        console.log("Dados recebidos do backend:", data);
+        this.authService.handleSetToken(data);
+        window.location.href = `/validacao-email?email=${dadosCadastro.email}`;
+      },
+      error: (err) => {
+        console.log("Erro ao tentar salvar o usuário:", err);
+
+        this.tipoAlerta = AlertType.Danger;
+        if (err.statusText === "Unknown Error") {
+          this.serverErrors.push(EMessages.SERVER_ERROR);
+        } else if (err.error && typeof err.error === 'string') {
+          this.serverErrors.push(err.error);
+        } else {
+          this.serverErrors.push("Ocorreu um erro desconhecido. Por favor, tente novamente.");
         }
-      });
+      
+        this.isLoading = false;
+        this.scrollTop();
+      }
+    });
   }
 
   handleChange(){
-    this.serverErrors = []
-    this.isSenhaValida()
+    this.serverErrors = [];
+    this.isSenhaValida();
   }
 
-  isSenhaValida(){
-    const senha = this.cadastroForm.controls.senha.value
-    const confirmPassword = this.cadastroForm.controls.confirmPassword.value
+  isSenhaValida() {
+    const senha = this.cadastroForm.controls.senha.value;
+    const confirmPassword = this.cadastroForm.controls.confirmPassword.value;
 
-    if(!senha || senha?.length < 8) {
-      this.serverErrors.push("Sua senha deve conter pelo menos 8 caracteres")
-      this.scrollTop()
+    console.log("Validando a senha...");
+
+    if (!senha || senha?.length < 8) {
+      console.log("Erro: Senha com menos de 8 caracteres.");
+      this.serverErrors.push("Sua senha deve conter pelo menos 8 caracteres");
+      this.scrollTop();
       return false;
     }
 
-    if(confirmPassword && senha !== confirmPassword) {
-      this.serverErrors.push("As senhas informadas não coincidem")
-      this.scrollTop()
+    if (confirmPassword && senha !== confirmPassword) {
+      console.log("Erro: Senha e confirmação de senha não coincidem.");
+      this.serverErrors.push("As senhas informadas não coincidem");
+      this.scrollTop();
       return false;
     }
 
@@ -122,9 +159,10 @@ export class CadastroComponent {
     this.serverErrors = [];
   };
 
-  scrollTop(){
-    window.scrollTo({ top: 0, behavior: 'smooth'})
+  scrollTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
+
   protected readonly CategoriaProjetoArr = CategoriasProjetoArr;
   protected readonly TipoUsuarioArr = TipoUsuarioArr;
   protected readonly ERROR_MESSAGES = ERROR_MESSAGES;
