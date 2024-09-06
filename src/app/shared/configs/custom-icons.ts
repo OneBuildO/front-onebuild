800a359aa5622d09c173e0ac76be145da8a02de3
vctrwesley <vctrwesley@gmail.com>
2024-09-06 12:12:00 -0300

    criando um componente para configurar os icones e atualizando os icones da sidebar

---
+++ src/app/shared/configs/custom-icons.ts
@@ -0,0 +1,35 @@
+export type customIcon = {
+  iconName: string;
+  iconPath: string;
+};
+
+export const iconsArray: customIcon[] = [
+  {
+    iconName: 'painel',
+    iconPath: './assets/icons/painel.svg',
+  },
+  {
+    iconName: 'clientes',
+    iconPath: './assets/icons/clients.svg',
+  },
+  {
+    iconName: 'project',
+    iconPath: './assets/icons/project.svg',
+  },
+  {
+    iconName: 'fornecedores',
+    iconPath: './assets/icons/store-white.svg',
+  },
+  {
+    iconName: 'config',
+    iconPath: './assets/icons/config.svg',
+  },
+  {
+    iconName: 'perfil-white',
+    iconPath: './assets/icons/perfil-white.svg',
+  },
+  {
+    iconName: 'logout',
+    iconPath: './assets/icons/sair.svg',
+  },
+];
