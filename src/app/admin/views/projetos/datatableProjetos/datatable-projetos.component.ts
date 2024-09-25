1e83bd1d90ac18e86c030897523f705dabfb01cc
vctrwesley <vctrwesley@gmail.com>
2024-09-25 16:32:40 -0300

    tabelas e modal delete da tela de projetos atualizada

--- src/app/admin/views/projetos/datatableProjetos/datatable-projetos.component.ts
+++ src/app/admin/views/projetos/datatableProjetos/datatable-projetos.component.ts
@@ -4,11 +4,12 @@ import { AdminRoutes } from "src/app/admin/admin.routes";
 import { formatDatePTBR } from 'src/app/shared/utils/Utils'
 import {ModalRemoveComponent} from "src/app/shared/components/modalRemove/modalRemove.component";
 import {ProjetoResumoDTO} from "src/app/_core/models/projeto.model";
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'datatable-projetos',
   standalone: true,
-  imports: [NgClass, NgIf, ModalRemoveComponent],
+  imports: [NgClass, NgIf, ModalRemoveComponent, MatIconModule],
   templateUrl: './datatable-projetos.component.html',
   styleUrl: './datatable-projetos.component.css',
 })
