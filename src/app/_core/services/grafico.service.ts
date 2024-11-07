5878988b83ee567b1524fbe331b7689c5a26a7bc
JVictor011 <jvictor2021@alu.ufc.br>
2024-11-07 10:55:59 -0300

    refatoração: adicionando token no service do grafico

--- src/app/_core/services/grafico.service.ts
+++ src/app/_core/services/grafico.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import {environment} from "../../../environments/environment";
+import { first, catchError } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GraficoService {
-    private readonly _apiBaseUrl = `${environment.apiUrl}`;
+  private readonly _apiBaseUrl = `${environment.apiUrl}`;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly authService: AuthService
+  ) {}
 
   getGraficoProjetosCadastro(): Observable<{ [key: string]: number }> {
-    return this.http.get<{ [key: string]: number }>(`${this._apiBaseUrl}/usuario/graficoProjetosCadastro`)
+    const headers = this.authService.generateHeader();
+    return this.httpClient
+      .get<{ [key: string]: number }>(
+        `${this._apiBaseUrl}/usuario/graficoProjetosCadastro`,
+        { headers }
+      )
       .pipe(
+        first(),
         catchError(this.handleError)
       );
   }
 
   private handleError(error: HttpErrorResponse) {
     console.error('Erro ao buscar dados do gráfico:', error);
-    return throwError('Erro ao buscar dados do gráfico. Tente novamente mais tarde.');
+    return throwError(
+      'Erro ao buscar dados do gráfico. Tente novamente mais tarde.'
+    );
   }
 }
