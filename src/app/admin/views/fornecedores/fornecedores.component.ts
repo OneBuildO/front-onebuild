d9ffe5fca88808d5a426180f1f11a6739947312b
JVictor011 <jvictor2021@alu.ufc.br>
2024-11-08 06:06:03 -0300

    Mapa em fornecedores

--- src/app/admin/views/fornecedores/fornecedores.component.ts
+++ src/app/admin/views/fornecedores/fornecedores.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 import { pageTransition } from 'src/app/shared/utils/animations';
-import {ModalComponent} from "../../../shared/components/modal/modal.component";
-import {DataTableComponent} from "../../../shared/components/data-table/data-table.component";
-import {IColumn, IProjects, TableData} from "../elements/data-table/table.data";
-import {NgClass, NgIf} from "@angular/common";
-import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from "@angular/forms";
-import {SpinnerComponent} from "../../../shared/components/spinner/spinner.component";
-import {AlertComponent} from "../../../shared/components/alert/alert.component";
-import {AlertType} from "../../../shared/components/alert/alert.type";
-import {ProjetoService} from "../../../_core/services/projeto.service";
+import { ModalComponent } from '../../../shared/components/modal/modal.component';
+import { DataTableComponent } from '../../../shared/components/data-table/data-table.component';
+import {
+  IColumn,
+  IProjects,
+  TableData,
+} from '../elements/data-table/table.data';
+import { NgClass, NgIf } from '@angular/common';
+import {
+  FormBuilder,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { SpinnerComponent } from '../../../shared/components/spinner/spinner.component';
+import { AlertComponent } from '../../../shared/components/alert/alert.component';
+import { AlertType } from '../../../shared/components/alert/alert.type';
+import { ProjetoService } from '../../../_core/services/projeto.service';
 Chart.register(...registerables);
 
 import { FornecedorService } from '../../../_core/services/fornecedor.service';
@@ -17,13 +26,13 @@ import { FornecedorDTO } from '../../../_core/models/fornecedor.model';
 import { CommonModule } from '@angular/common';
 
 import { ChartComponent } from '../../../shared/components/chart/chart.component';
-
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
-    selector: 'app-fornecedores',
-    templateUrl: './fornecedores.component.html',
-    styleUrls: ['./fornecedores.component.css'],
-    standalone: true,
+  selector: 'app-fornecedores',
+  templateUrl: './fornecedores.component.html',
+  styleUrls: ['./fornecedores.component.css'],
+  standalone: true,
   imports: [
     ModalComponent,
     DataTableComponent,
@@ -35,13 +44,17 @@ import { ChartComponent } from '../../../shared/components/chart/chart.component
     CommonModule,
     ChartComponent,
   ],
-    animations: [pageTransition]
+  animations: [pageTransition],
 })
 export class FornecedoresComponent implements OnInit {
-  
+  map!: mapboxgl.Map;
+  style = 'mapbox://styles/mapbox/streets-v11';
+  mapBoxApiKey =
+    'pk.eyJ1IjoianZpY3RvcjAxMSIsImEiOiJjbTJrdDF3bjEwNHZuMmxvZXFzbmR2ZXZjIn0.i54Xkjxm3rb7vYvXt2Sq_w';
+
   constructor(
     private formBuilder: FormBuilder,
-    private projectService : ProjetoService,
+    private projectService: ProjetoService,
     private fornecedorService: FornecedorService
   ) {
     this.modalCompnent = new ModalComponent();
@@ -50,7 +63,7 @@ export class FornecedoresComponent implements OnInit {
   // public selectedProjet?: ProjetoModelDTO | null;
   public projects: IProjects[] = TableData.projects;
   public pages: number[] = TableData.pageNumber;
-  public columnData:IColumn[] = TableData.columnData;
+  public columnData: IColumn[] = TableData.columnData;
   public fornecedores: FornecedorDTO[] = [];
   selectedFornecedor: FornecedorDTO | null = null;
   showModal: boolean = false;
@@ -58,28 +71,41 @@ export class FornecedoresComponent implements OnInit {
   isLoading: boolean = false;
   serverMessages: string[] = [];
   tipoAlerta = AlertType.Warning;
-  modalCompnent: ModalComponent
+  modalCompnent: ModalComponent;
 
   showDetailModal: boolean = false;
 
   projectForm = this.formBuilder.group({
-    arquivo: new FormControl( '', ),
-    nomeProjeto: new FormControl( '', { validators: [Validators.required] }),
-    nomeCliente: new FormControl( '', { validators: [Validators.required] }),
-    dataLimiteOrcamento: new FormControl( '', { validators: [Validators.required] }),
+    arquivo: new FormControl(''),
+    nomeProjeto: new FormControl('', { validators: [Validators.required] }),
+    nomeCliente: new FormControl('', { validators: [Validators.required] }),
+    dataLimiteOrcamento: new FormControl('', {
+      validators: [Validators.required],
+    }),
   });
 
   ngOnInit(): void {
     this.getFornecedores();
     this.projects = [];
-    this.projectService.getAllProjectForUser()
-      .subscribe({
-        next: (data: any) => {
-          this.projects = data
-        },
-        error: (err) => {
-        }
-      });
+    this.projectService.getAllProjectForUser().subscribe({
+      next: (data: any) => {
+        this.projects = data;
+      },
+      error: (err) => {},
+    });
+  }
+
+  ngAfterViewInit(): void {
+    (mapboxgl as any).accessToken = this.mapBoxApiKey;
+
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      center: [-38.528, -3.728],
+      zoom: 12,
+    });
+
+    this.map.addControl(new mapboxgl.NavigationControl());
   }
 
   handleModal() {
@@ -106,9 +132,9 @@ export class FornecedoresComponent implements OnInit {
 
   protected onFormSubmitHandler = () => {
     this.submited = true;
-    this.serverMessages = []
+    this.serverMessages = [];
 
-    if(this.projectForm.invalid) return
+    if (this.projectForm.invalid) return;
 
     // const dadosProjeto : ProjetoModelDTO = {
     //   nome: this.projectForm.controls?.nomeProjeto?.value,
@@ -133,24 +159,26 @@ export class FornecedoresComponent implements OnInit {
     //   });
   };
 
-  handleView(){
-    console.log('view')
+  handleView() {
+    console.log('view');
   }
 
-  handleEdit(){
-    console.log('Edit')
+  handleEdit() {
+    console.log('Edit');
   }
 
-  handleRemove(){
-    console.log('Remove')
+  handleRemove() {
+    console.log('Remove');
   }
   protected onAlertCloseHandler = (e: any) => {
     this.serverMessages = [];
   };
 
   getFornecedores(): void {
-    this.fornecedorService.getFornecedores().subscribe((data: FornecedorDTO[]) => {
-      this.fornecedores = data;
-    });
+    this.fornecedorService
+      .getFornecedores()
+      .subscribe((data: FornecedorDTO[]) => {
+        this.fornecedores = data;
+      });
   }
 }
