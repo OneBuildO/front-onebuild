8afd4533e277a42e12c8346186a0df6ba0cc2e99
JVictor011 <jvictor2021@alu.ufc.br>
2024-11-22 04:21:49 -0300

    refatoração: passando os dados do cadastro de promoção pelo body

--- src/app/_core/services/oferta.service.ts
+++ src/app/_core/services/oferta.service.ts
@@ -1,36 +1,41 @@
-import {Injectable} from '@angular/core';
-import {environment} from "../../../environments/environment";
-import {HttpClient} from "@angular/common/http";
-import {first, Observable} from "rxjs";
-import {AuthService} from "./auth.service";
-import {MinhasOfertasDTO} from "../models/oferta.model";
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { first, Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { MinhasOfertasDTO } from '../models/oferta.model';
 
 @Injectable({ providedIn: 'root' })
 export class OfertaService {
-
   private readonly _apiBaseUrl = `${environment.apiUrl}`;
 
   constructor(
     private readonly httpClient: HttpClient,
-    private readonly authService : AuthService,
+    private readonly authService: AuthService
   ) {}
 
-  saveNewOferta(newOferta: MinhasOfertasDTO, files: File[], idPromocao: number | null): Observable<any> {
-    const formData = new FormData();
-    files.map((file) => formData.append('arquivo', file));
-    formData.append("reportProgress", "true");
-    formData.append("newOferta", JSON.stringify(newOferta));
-  
-    return this.httpClient.post(`${this._apiBaseUrl}/promocao`, formData, {
-      responseType: "text",
-      headers: this.authService.generateHeader()
-    }).pipe(first());
+  saveNewOferta(
+    newOferta: MinhasOfertasDTO,
+    files: File[],
+    idPromocao: number | null
+  ): Observable<any> {
+    return this.httpClient
+      .post(`${this._apiBaseUrl}/promocoes`, newOferta, {
+        responseType: 'text',
+        headers: this.authService.generateHeader(),
+      })
+      .pipe(first());
   }
 
   editOferta(newOferta: MinhasOfertasDTO): Observable<any> {
-    return this.httpClient.put<any>(`${this._apiBaseUrl}/editar/promocao/${newOferta.id}`, newOferta, {
-        headers: this.authService.generateHeader()
-      }).pipe(first());
+    return this.httpClient
+      .put<any>(
+        `${this._apiBaseUrl}/editar/promocao/${newOferta.id}`,
+        newOferta,
+        {
+          headers: this.authService.generateHeader(),
+        }
+      )
+      .pipe(first());
   }
-  
 }
